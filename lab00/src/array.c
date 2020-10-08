444138ea43531df1feea48a3107c0b4909f5a04b
Daniel Morandini <danielmorandini@me.com>
2020-10-08 22:58:24 +0200

    Measure using measure.h

--- lab00/src/array.c
+++ lab00/src/array.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <time.h>
+#include "measure.h"
 
 void
 usage(char* name) {
@@ -8,10 +10,11 @@ usage(char* name) {
 }
 
 int
-main(int argc, char* argv[argc+1]) {
-	int *a;
-	int *b;
-	int *c;
+main(int argc, char *argv[]) {
+	int err;
+	struct timespec t;
+	duration elapsed;
+	int *a, *b, *c;
 
 	if (argc != 2) {
 		usage(argv[0]);
@@ -26,15 +29,26 @@ main(int argc, char* argv[argc+1]) {
 	a = malloc(sizeof(int)*n);
 	b = malloc(sizeof(int)*n);
 	c = malloc(sizeof(int)*n);
+
+	if ((err = now(&t))) {
+		perror("now");
+		return err;
+	}
 	for (int i = 0; i < n; i++) {
 		a[i] = rand() % INT_MAX/2;
 		b[i] = rand() % INT_MAX/2;
 		c[i] = a[i] + b[i];
 	}
-
+	if ((elapsed = sub(&t)) < 0) {
+		perror("sub");
+		return elapsed;
+	}
 	for (int i = 0; i < n; i++) {
 		fprintf(stdout, "%d\n", c[i]);
 	}
+
+	fprintf(stderr, "n=%d\n", n);
+	fprintf(stderr, "elapsed=%.3fms\n", ms(elapsed));
 
 	free(a);
 	free(b);

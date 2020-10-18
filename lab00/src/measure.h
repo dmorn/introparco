c7b0c78a687583767c2f7581c2b2d13372c8f9f9
Daniel Morandini <danielmorandini@me.com>
2020-10-18 11:41:39 +0200

    Add release helpers, rename proj root

--- lab00/src/measure.h
+++
@@ -1,7 +0,0 @@
-#include <time.h>
-#include <stdlib.h>
-
-/* time interval since epoch in nanoseconds.
- * Exits if an error is encountered, hence it
- * just suitable for benchmarking. */
-long now_ns();

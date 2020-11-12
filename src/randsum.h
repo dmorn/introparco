13cc90481f02bf960cf270c47c457d00a5adcd09
Daniel Morandini <danielmorandini@me.com>
2020-11-12 17:10:34 +0100

    Fix compilation problems

--- src/randsum.h
+++ src/randsum.h
@@ -1,3 +1,9 @@
 #include <stdint.h>
 
-void randsum(int n, uint32_t *a, uint32_t *b, uint32_t *c);
+#ifdef __cplusplus
+#define LINKAGE "C"
+#else
+#define LINKAGE
+#endif
+
+extern LINKAGE void randsum(int n, uint32_t *a, uint32_t *b, uint32_t *c);

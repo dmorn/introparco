ad3b22110d9635d91e5391594634238d1d1b0f19
Daniel Morandini <danielmorandini@me.com>
2020-11-16 20:14:24 +0100

    MOve scripts in this dedicated folder

---
+++ scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+USER=${USER:-dmorandini}
+HOST=${HOST:-slurm-ctrl.inf.unibz.it}
+ADDR=${USER}@${HOST}
+
+export REV=${REV:-$(git rev-parse --short HEAD)}
+export RELNAME=release-${REV}
+export RELARCHIVE=${RELNAME}.tar.gz
+
+DIR=$(dirname "$0")
+HOSTREL=./${RELNAME}
+
+(cd ${DIR}/.. && make release && mv ${RELARCHIVE} $DIR)
+scp ${DIR}/${RELARCHIVE} ${ADDR}:.
+ssh ${ADDR} "rm -r ${HOSTREL} 2>/dev/null"
+ssh ${ADDR} "mkdir -p ${HOSTREL} 2>/dev/null"
+ssh ${ADDR} "tar -zxf ${RELARCHIVE} -C ${HOSTREL}"
+
+rm ${DIR}/${RELARCHIVE}
+ssh ${ADDR} "rm ${RELARCHIVE}"
+
+echo 🚀 ${ADDR}:${HOSTREL}
---
+++ scripts/srun.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# runs benchmark using slurm's job system. No huge automation, just some
+# defaults.
+
+# stderr & stdout are redirected by default to caller's stderr & stdout.
+# input should come from stdin.
+srun \
+	--partition training \
+	--gres=gpu \
+	--mem-per-cpu=4gb \
+	--nodes=1 \
+	--time=00:01:00 \
+	--ntasks=1 \
+	-N 1 \
+	$@

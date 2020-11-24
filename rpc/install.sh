038af4c17cbe6ccbdaa202b16d7b63528a8b4b03
Daniel Morandini <danielmorandini@me.com>
2020-11-24 18:10:54 +0100

    Add rpc installation script

---
+++ rpc/install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# tars the project and scp's it to a remote system, untars, makes
+# everything and cleans up archives.
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/../env.sh
+
+make -C $DIR/.. -i clean 1>/dev/null
+make -C $DIR/.. ${ARCHIVE}
+function cleanup() {
+	rm -rf ${DIR}/../${ARCHIVE}
+}
+trap cleanup EXIT
+
+scp ${DIR}/../${ARCHIVE} ${ADDR}:.
+
+ssh $ADDR $(cat << EOF
+	# this is very tight to our system details.
+	source /etc/profile && \
+	module load cuda-10.2 gcc-6.5.0 && \
+
+	rm -rf ${HOSTREL} && \
+	mkdir -p ${HOSTREL} && \
+	tar -zxf ${ARCHIVE} -C ${HOSTREL} && \
+	make -C ${HOSTREL} all && \
+	rm -rf ${ARCHIVE}
+EOF
+)
+
+echo 🚀 ${ADDR}:${HOSTREL}
